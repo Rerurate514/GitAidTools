71bda9020ec03fdc02dd48e2272522fa087819a9
Rerurate514 <rerurateyuto@gmail.com>
2023-06-21 23:25:47 +0900

    logics-file, github_api.rs added

---
+++ src-tauri/src/logics/github_api.rs
@@ -0,0 +1,6 @@
+pub mod github_api{
+    pub fn test_authentication_api(url: &str){
+        println!("{}",url);
+    }
+}
+
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod logics;
+use logics::github_api::github_api::test_authentication_api;
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,7 +13,15 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            test_authentication
+            ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn test_authentication(url: &str){
+    test_authentication_api(url)
 }
