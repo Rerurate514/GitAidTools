bf0cfd73c8b504fad10828c3a2a977e8e3242418
Rerurate514 <rerurateyuto@gmail.com>
2023-06-23 18:18:30 +0900

    option and json code added

--- src-tauri/src/logics/github_api.rs
+++ src-tauri/src/logics/github_api.rs
@@ -1,6 +1,27 @@
 pub mod github_api_test{
-    pub fn test_authentication_api(url: &str){
-        println!("{}",url);
+    use reqwest::{ClientBuilder};
+    use serde_json::json;
+
+    #[tokio::main]
+    pub async fn test_authentication_api(_url: &str) -> Result<(), Box<dyn std::error::Error>>{
+        let option = json!({
+            "url" : _url,
+            "header" : {
+                "User-Agent" : "request"
+            }            
+        });
+
+        let client = ClientBuilder::new()
+            .user_agent("request")
+            .build()?;
+        
+        println!("fetch url : {}",_url);
+        println!("option ::: Json => {}",option);
+        
+        let res = client.get( _url ).send().await? ;
+        let body = res.text().await?;
+
+        println!("response : \n{}",body);
+        Ok(())
     }
 }
-
