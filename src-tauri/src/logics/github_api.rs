aac98ec3f0c7b9ae55ef044597f6d1f32aab9cde
Rerurate514 <rerurateyuto@gmail.com>
2023-06-24 23:39:49 +0900

    test ->oauth code changed, open_web_view code added

--- src-tauri/src/logics/github_api.rs
+++ src-tauri/src/logics/github_api.rs
@@ -1,63 +1,29 @@
-pub mod github_api_test{
+pub mod github_api_auth{
     use tauri::WindowBuilder;
     use tauri::WindowUrl;
     use octocrab::Octocrab;
     const CLIENT_ID : &str = "13148b29f0040e5807ac";
     const CLIENT_SECRET: &str = "3c5f2eec1222e7d46e4fbc448c19089343c000bc";
 
-    #[tokio::main]// -> octocrab::Result<()>
-    pub async fn test_authentication_api(_url: &str){
+    // -> octocrab::Result<()>
+    pub fn oauth_request_api(app: tauri::AppHandle){
         let temp_url = format!("{}{}{}","https://github.com/login/oauth/authorize?client_id=",CLIENT_ID,"&scope=repo");
+        let temp_url_fn = temp_url.clone();
         println!("{}",temp_url);
 
-        open_web_view(&temp_url);
-        
-        let response = reqwest::get(temp_url).await;
-        match response {
-            Ok(res) => {
-                let body = res.text().await;
-                println!("res : {:?}",body);
-            }
-            Err(err) => {
-                println!("ERORR!");
-            }
-        }
+        open_web_view(app,temp_url_fn);
     }
 
-    #[tokio::main]
-    async fn open_web_view(_url : &str){
-        let url = _url;
-        tauri::Builder::default()
-            .setup(|app| {
-                let window = WindowBuilder::new(
-                    app,
-                    "OAuth認証",
-                    WindowUrl::App(url.into())
-                ).build()?;
-                Ok(())
-            });
+    fn open_web_view(app: tauri::AppHandle, _url: String){
+        let window = tauri::WindowBuilder::new(
+            &app, 
+            "OAuth認証", 
+            tauri::WindowUrl::External(_url.parse().unwrap())
+        )
+        .build()
+        .unwrap();
+
+        println!("openWEv");
     }
 
-    //#[tokio::main]
-    // pub async fn test_authentication_api(_url: &str) -> Result<(), Box<dyn std::error::Error>>{
-    //     let option = json!({
-    //         "url" : _url,
-    //         "header" : {
-    //             "User-Agent" : "request"
-    //         }            
-    //     });
-
-    //     let client = ClientBuilder::new()
-    //         .user_agent("request")
-    //         .build()?;
-        
-    //     println!("fetch url : {}",_url);
-    //     println!("option ::: Json => {}",option);
-        
-    //     let res = client.get( _url ).send().await? ;
-    //     let body = res.text().await?;
-
-    //     println!("response : \n{}",body);
-    //     Ok(())
-    // }
 }
