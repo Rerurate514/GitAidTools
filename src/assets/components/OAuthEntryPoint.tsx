eb4c86a9776f5c97736e8568de88454aa2166bcc
Rerurate514 <rerurateyuto@gmail.com>
2023-06-25 00:47:57 +0900

    pageDesign code added

--- src/assets/components/OAuthEntryPoint.tsx
+++ src/assets/components/OAuthEntryPoint.tsx
@@ -1,4 +1,5 @@
 import { invoke } from "@tauri-apps/api"
+import githubLogo from "../image/GitHub-Logo.png"
 
 export const OAuthRequest = () => {
     async function oauth_request() {
@@ -7,12 +8,37 @@ export const OAuthRequest = () => {
 
     return(
         <div>
-            <div style={{height: '100%'}}>
-                <div style={{textAlign: 'center', verticalAlign: 'middle', height: '100%'}}>
-                <h2 style={{marginTop: '16px'}}>最初にGithubとの連携を行います。</h2>
-                <button onClick={oauth_request}>認証する</button>
-                <h5 style={{marginTop: '16px'}}>このアプリケーションはOAuth認証を採用しています。</h5>
-            </div>
+            <div style={{
+                    position: 'absolute',
+                    top: '0',
+                    bottom: '0',
+                    right: '0',
+                    left: '0',
+                    margin: 'auto',
+                    // width: '300px',
+                    //height: '200px',
+                }}>
+                    <div style={{
+                            textAlign: 'center',
+                            background: '#444444',
+
+                            marginTop: '12%',
+                            marginBottom: '25%',
+                            marginRight: '20%',
+                            marginLeft: '20%',
+
+                            borderWidth: '4px',
+                            borderColor: '#CCCCCC',
+                            borderStyle: 'solid',
+                            borderRadius: '7px'
+                        }}>
+                        <h2 style={{marginTop: '16px'}}>最初にGithubとの連携を行います。</h2>
+                        <div style={{margin: '16px'}}>
+                            <img src={githubLogo} style={{width: '45%'}}></img>
+                        </div>
+                        <button onClick={oauth_request}>認証する</button>
+                        <h5>このアプリケーションはOAuth認証を採用しています。</h5>
+                    </div>
             </div>
         </div>
     )
