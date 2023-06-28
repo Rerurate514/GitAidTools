e0a1213d76030e66cb1678f18c9e24c2f0ea9eea
Rerurate514 <rerurateyuto@gmail.com>
2023-06-28 23:22:57 +0900

    ステージングファイルの追加するformの追加

---
+++ src/assets/components/InputStageFile.tsx
@@ -0,0 +1,27 @@
+import { invoke } from "@tauri-apps/api";
+import { useState } from "react";
+
+
+export const InputAddFile = () => {
+    const [path, setPath] = useState("");
+
+    async function git_add_cmd() {
+        await invoke("git_add_cmd",{ path })
+    }
+
+    return(
+        <div>
+            <h3>１．ここにはステージングしたいファイルかディレクトリの絶対パスを入力してください。</h3>
+            <div style={{display: 'flex'}}>
+                <form
+                onSubmit={(e) => {
+                    e.preventDefault(),
+                    git_add_cmd()
+                }}>
+                    <input style={{width: '650px'}} onChange={(e) => {setPath(e.currentTarget.value)}} placeholder="Enter add file"></input>
+                    <button type="submit" style={{marginLeft: '8px'}}>add</button>
+                </form>
+            </div>
+        </div>
+    )
+}
